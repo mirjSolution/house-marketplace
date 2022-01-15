262f34b2332dae2ee9c8a07ce6c0ebd25af04414
Rico John Dato-on <ricojohndatoon23@gmail.com>
2022-01-15 18:21:53 -0500

    Explore slider

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg';
 import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg';
+import Slider from '../components/Slider';
 
 function Explore() {
   return (
@@ -8,7 +9,9 @@ function Explore() {
       <header>
         <p className='pageHeader'>Explore</p>
       </header>
-      <main>{/* Slider */}</main>
+      <main>
+        <Slider />
+      </main>
       <p className='exploreCategoryHeading'>Categories</p>
       <div className='exploreCategories'>
         <Link to='/category/rent'>
